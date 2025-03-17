80c7307e249268774fa68ca1fd6c1b9c032d3978
Jubayer Hasan Munna <185611999+vexlore-code@users.noreply.github.com>
2025-03-18 02:19:45 +0600

    Problem02.c

---
+++ 2nd Problem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.141592653589793
+
+int main()
+{
+    int m1 = 3, m2 = 2;
+    double l1 = 530, l2 = 700, th1 = 65.0, s2, th2;
+
+    s2 = (m2 * l2) / (m1 * l1) * sin(th1 * PI / 180);
+    
+    if (s2 > 1.0) printf("No valid angle exists.\n");
+    else
+    {
+        th2 = asin(s2) * (180.0 / PI);
+        printf("Angle for red light: %.2f°\n", th2);
+    }
+
+    return 0;
+}
