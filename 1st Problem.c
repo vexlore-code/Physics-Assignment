bb28b506db9ec0af8f60220ee9f9bed346cf2f09
Jubayer Hasan Munna <185611999+vexlore-code@users.noreply.github.com>
2025-03-18 02:19:02 +0600

    Problem01.c

---
+++ 1st Problem.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.141592653589793
+
+int main()
+{
+    int m;
+    double th, d, l;
+
+    printf("Enter m, θ (deg), d (μm): ");
+    scanf("%d %lf %lf", &m, &th, &d);
+
+    d *= 1e-6; 
+    l = (d * sin(th * PI / 180)) / m * 1e9; 
+
+    printf("Wavelength: %.2f nm\n", l);
+
+    if (l >= 380 && l < 450) printf("Color: Violet\n");
+    else if (l >= 450 && l < 485) printf("Color: Blue\n");
+    else if (l >= 485 && l < 500) printf("Color: Cyan\n");
+    else if (l >= 500 && l < 565) printf("Color: Green\n");
+    else if (l >= 565 && l < 590) printf("Color: Yellow\n");
+    else if (l >= 590 && l < 625) printf("Color: Orange\n");
+    else if (l >= 625 && l <= 750) printf("Color: Red\n");
+    else printf("Color: Out of Visible Spectrum\n");
+
+    return 0;
+}
